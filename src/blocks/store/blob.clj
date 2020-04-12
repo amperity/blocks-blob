4ffc153b6fced72f386517771433fc2f5c94be11
Greg Look <greg@mvxcvi.com>
2020-04-12 08:56:43 -0700

    Rewrite blob store.

--- src/blocks/store/blob.clj
+++ src/blocks/store/blob.clj
@@ -7,7 +7,8 @@
     [clojure.string :as str]
     [clojure.tools.logging :as log]
     [com.stuartsierra.component :as component]
-    [multihash.core :as multihash])
+    [manifold.stream :as s]
+    [multiformats.hash :as multihash])
   (:import
     (com.microsoft.azure.storage
       StorageCredentials
@@ -19,13 +20,24 @@
       CloudBlobClient
       CloudBlobContainer
       CloudBlockBlob
+      ListBlobItem
       SharedAccessBlobHeaders
       SharedAccessBlobPolicy)
     (java.io
       InputStream
       OutputStream)
-    (java.net
-      URI)))
+    java.net.URI
+    java.time.Instant
+    (org.apache.commons.io.input
+      BoundedInputStream)))
+
+
+;; ## Blob Functions
+
+(defn- hex?
+  "True if the value is a valid hexadecimal string."
+  [x]
+  (and (string? x) (re-matches #"[0-9a-fA-F]+" x)))
 
 
 (defn- id->path
@@ -36,28 +48,96 @@
   (str root (multihash/hex id)))
 
 
-(defn blob->stats
+(defn- blob->stats
+  "Return the stats map (with metadata) for a blob."
   [^CloudBlob blob]
-  (let [^BlobProperties properties (.getProperties blob)
-        name (last (str/split (.getName blob) #"/"))]
-    {:id (multihash/decode name)
-     :size (.getLength properties)
-     :source (.getPrimaryUri (.getSnapshotQualifiedStorageUri blob))
-     :stored-at (.getLastModified properties)}))
+  (let [properties (.getProperties blob)
+        blob-name (last (str/split (.getName blob) #"/"))]
+    (when (hex? blob-name)
+      (with-meta
+        {:id (multihash/parse blob-name)
+         :size (.getLength properties)
+         :stored-at (.toInstant (.getLastModified properties))}
+        {::source (.getPrimaryUri (.getSnapshotQualifiedStorageUri blob))
+         ::name (.getName blob)}))))
 
 
-(defn file->block
-  [^CloudBlobContainer container root stats]
-  (let [id (:id stats)
-        path (id->path root id)]
-    (block/with-stats
-      (data/lazy-block
-        id (:size stats)
-        (fn file-reader
-          []
-          (-> (.getBlockBlobReference container path)
-              (.openInputStream))))
-      (dissoc stats :id :size))))
+
+;; ## Blob Content
+
+(deftype BlobReader
+  [^CloudBlockBlob blob]
+
+  data/ContentReader
+
+  (read-all
+    [this]
+    (log/trace "Opening Azure blob" (.getName blob))
+    #_(.getBlockBlobReference container blob-name)
+    (.openInputStream blob))
+
+
+  (read-range
+    [this start end]
+    (log/tracef "Opening Azure blob %s byte range %s - %s"
+                (.getName blob)
+                (or start "start")
+                (or end "end"))
+    (.openInputStream blob
+                      (long (or start 0))
+                      (and end (- end (or start 0)))
+                      nil nil nil)))
+
+
+(alter-meta! #'->BlobReader assoc :private true)
+
+
+(defn- blob->block
+  "Construct a new block from the given block blob reference."
+  [blob stats]
+  (let [stat-meta (meta stats)]
+    (with-meta
+      (data/create-block
+        (:id stats)
+        (:size stats)
+        (:stored-at stats)
+        (->BlobReader blob))
+      stat-meta)))
+
+
+;; ## Blob Functions
+
+(defn- run-blobs!
+  "Run the provided function over all blobs in the container matching the given
+  query. The loop will terminate if the function returns false or nil."
+  [^CloudBlobContainer container path query f]
+  (let [{:keys [after before]} query]
+    ;; OPTIMIZE: if after/before share a common prefix we can narrow the range here.
+    (loop [blobs (.iterator (.listBlobs container path true))
+           limit (:limit query)]
+      (when-let [blob (and (or (nil? limit) (pos? limit))
+                           (.hasNext blobs)
+                           (.next blobs))]
+        (if-let [stats (blob->stats blob)]
+          (cond
+            ;; Skip block which occurs before :after marker.
+            (and after (not (pos? (compare (multihash/hex (:id stats)) after))))
+            (recur blobs limit)
+
+            ;; Terminate when encountering blocks after :before marker.
+            (and before (not (neg? (compare (multihash/hex (:id stats)) before))))
+            nil
+
+            ;; Otherwise, call function.
+            (f blob)
+            (recur blobs (dec limit))
+
+            ;; Function returned false, halt iteration.
+            :else nil)
+          ;; Ignore non-block blob.
+          (recur blobs limit))))))
+
+
 
 ;; ## Block Store
 
@@ -72,7 +152,9 @@
   (start
     [this]
     (if-not container
-      (assoc this :container (CloudBlobContainer. container-uri credentials))
+      (do
+        (log/info "Connecting to Azure blob container" container-uri)
+        (assoc this :container (CloudBlobContainer. container-uri credentials)))
       this))
 
 
@@ -85,50 +167,81 @@
 
   store/BlockStore
 
-  (-stat
-    [this id]
-    (let [path (id->path root id)
-          ^CloudBlob blob (.getBlockBlobReference container path)]
-      (when (and blob (.exists blob))
-        (blob->stats blob))))
-
-
   (-list
     [this opts]
-    (->> (.listBlobs container root true) ; lazy, flat list of blobs
-         (map blob->stats)
-         (store/select-stats opts)))
+    (let [out (s/stream 1000)]
+      (store/future'
+        (try
+          (run-blobs!
+            container root opts
+            (fn stream-block
+              [blob]
+              (if-let [stats (and (instance? CloudBlockBlob blob)
+                                  (blob->stats blob))]
+                ;; Publish block to stream.
+                @(s/put! out (blob->block blob stats))
+                ;; Doesn't look like a block - ignore and continue.
+                true)))
+          (catch Exception ex
+            (log/error ex "Failure listing blob container")
+            (s/put! out ex))
+          (finally
+            (s/close! out))))
+      (s/source-only out)))
+
+
+  (-stat
+    [this id]
+    (store/future'
+      (let [path (id->path root id)
+            blob (.getBlockBlobReference container path)]
+        (when (and blob (.exists blob))
+          (blob->stats blob)))))
 
 
   (-get
     [this id]
-    (when-let [stats (.-stat this id)]
-      (file->block container root stats)))
+    (store/future'
+      (let [path (id->path root id)
+            blob (.getBlockBlobReference container path)]
+        (when-let [stats (and blob (.exists blob) (blob->stats blob))]
+          (blob->block blob stats)))))
 
 
   (-put!
     [this block]
-    (let [path (id->path root (:id block))
-          ^CloudBlob blob (.getBlockBlobReference container path)]
-      (when-not (.exists blob)
-        (with-open [output (.openOutputStream blob)
-                    content (block/open block)]
-          (io/copy content output)))
-      (.-get this (:id block))))
+    (store/future'
+      (let [path (id->path root (:id block))
+            blob (.getBlockBlobReference container path)]
+        (if (.exists blob)
+          ;; Read existing blob.
+          (let [stats (blob->stats blob)]
+            (blob->block blob stats))
+          ;; Write blob.
+          (do
+            (with-open [output (.openOutputStream blob)
+                        content (data/content-stream block nil nil)]
+              (io/copy content output))
+            (let [blob (.getBlockBlobReference container path)
+                  stats (blob->stats blob)]
+              (blob->block blob stats)))))))
 
 
   (-delete!
     [this id]
-    (try
-      (let [path (id->path root id)
-            ^CloudBlob blob (.getBlockBlobReference container path)]
-        (log/debugf "Deleting file %s" (.getPrimaryUri (.getSnapshotQualifiedStorageUri blob)))
-        (.delete blob)
-        true)
-      (catch StorageException se
-        (when (not= 404 (.getHttpStatusCode se))
-          (throw se))
-        false))))
+    (store/future'
+      (try
+        (let [path (id->path root id)
+              blob (.getBlockBlobReference container path)]
+          (log/debugf "Deleting file %s" (.getPrimaryUri (.getSnapshotQualifiedStorageUri blob)))
+          (.delete blob)
+          true)
+        (catch StorageException se
+          (when (not= 404 (.getHttpStatusCode se))
+            (throw se))
+          false)))))
+
+
 
 ;; ## Store Construction
 
@@ -150,7 +263,7 @@
 
 
 (defn blob-block-store
-  [container-uri ^StorageCredentials credentials & {:as opts}]
+  [container-uri credentials & {:as opts}]
   (map->BlobBlockStore
     (assoc opts
            :credentials credentials
